9c32f6f94c4549228e8fa4f993f54e2119efff2b
mehrdad <mehrdadosoulisaran@gmail.com>
2024-05-08 13:08:02 +0330

    update config tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode:'class',
+  darkMode: "class",
   theme: {
     colors: {
       //- primary & secondary color
@@ -53,43 +53,31 @@ export default {
       lineSecondaryColor: "#F3F4F6", //- gray 100
       lineAccentColor: "#FED7AA", //- orange 200
       // lineColor: "#ffffff/10", //- white/10
-      "Brown": {
-        900:"#634832",
-        600:"#967259",
-        300:"#DBC1AC",
-        100:"#ECE0D1",
+      Brown: {
+        900: "#634832",
+        600: "#967259",
+        300: "#DBC1AC",
+        100: "#ECE0D1",
       },
-      "white-10":"rgba(255, 255, 255, 0.10)"
-      ,
-      "orange-lights":"rgba(254, 215, 170, 0.20)"
-      ,
-      "menu":"rgba(0, 0, 0, 0.50)",
-      "lightnes":"rgba(0, 0, 0, 0.40)"
+      "white-10": "rgba(255, 255, 255, 0.10)",
+      "orange-lights": "rgba(254, 215, 170, 0.20)",
+      menu: "rgba(0, 0, 0, 0.50)",
+      lightnes: "rgba(0, 0, 0, 0.40)",
     },
-    backgroundColor:{
-      "darken":"#27272A",
-      "btn":"linear-gradient(270deg, #FDBA74 0%, #FED7AA 100%);"
+    backgroundColor: {
+      darken: "#27272A",
+      btn: "linear-gradient(270deg, #FDBA74 0%, #FED7AA 100%);",
     },
     boxShadow: {
-      "Light":"0px 1px 10px 0px rgba(0, 0, 0, 0.05)"
-    },
-    borderColor: {
-      "4xl":"2rem"
-    },
-    boxShadow:{
-      "menu":"0px 1px 10px 0px rgba(0, 0, 0, 0.05)"
-    },
-    boxShadow:{
-      "lights":"0px 0px 10px 0px rgba(0, 0, 0, 0.04)"
-    },
-    dropShadow:{
-      "lights":"0px 0px 10px 0px rgba(0, 0, 0, 0.04)"
+      Light: "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+      menu: "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+      lightness: "0px 0px 10px 0px rgba(0, 0, 0, 0.04)",
     },
     letterSpacing: {
-      "tracking-tighter":"0.065em"
+      "tracking-tighter": "0.065em",
     },
-    borderColor:{
-      darkBorder:"rgba(255, 255, 255, 0.10)"
+    borderColor: {
+      darkBorder: "rgba(255, 255, 255, 0.10)",
     },
     fontFamily: {
       dana: "Dana",
@@ -100,26 +88,27 @@ export default {
       morabbamedium: "Morabba Medium",
     },
     extend: {
-      backgroundImage:{
-        "desctop-image":"url(../src/image/headerBgDesktop.webp)",
-        "mobile-image":"url(../src/image/headerBgMobile.webp)",
-        "products":"url(../src/image/body-bg.png)",
-        "bottom-product-right":"url(../src/image/categories/category-right.jpg)",
-        "bottom-product-left":"url(../src/image/categories/category-left.jpg)",
-        "orangess":"linear-gradient(270deg, #FDBA74 0%, #FED7AA 100%)",
-        "article1":"url(../src/image/blogs/blog-1.png);",
-        "article2":"url(../src/image/blogs/blog-2.png);",
-        "article3":"url(../src/image/blogs/blog-3.png);",
-        "article4":"url(../src/image/blogs/blog-4.png);",
-        "article5":"url(../src/image/contact.png);",
+      backgroundImage: {
+        "desctop-image": "url(../src/image/headerBgDesktop.webp)",
+        "mobile-image": "url(../src/image/headerBgMobile.webp)",
+        products: "url(../src/image/body-bg.png)",
+        "bottom-product-right":
+          "url(../src/image/categories/category-right.jpg)",
+        "bottom-product-left": "url(../src/image/categories/category-left.jpg)",
+        orangess: "linear-gradient(270deg, #FDBA74 0%, #FED7AA 100%)",
+        article1: "url(../src/image/blogs/blog-1.png);",
+        article2: "url(../src/image/blogs/blog-2.png);",
+        article3: "url(../src/image/blogs/blog-3.png);",
+        article4: "url(../src/image/blogs/blog-4.png);",
+        article5: "url(../src/image/contact.png);",
       },
-      screens:{
-        'xs':"480px",
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-      }
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
     },
   },
   plugins: [],
