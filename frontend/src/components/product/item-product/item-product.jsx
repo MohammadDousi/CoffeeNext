24c054cc87406064e371b5b0a322cadabfcb7ca1
mohammad dousi <m.dousi96@gmail.com>
2024-05-08 19:54:09 +0330

    install hero icon , debug color code in config tailwind - create font.css in folder fonts and import file in app.css - create base style in index.css - create and set style item product in components - create title section and set style - create categoryleft and right in home page

---
+++ frontend/src/components/product/item-product/item-product.jsx
@@ -0,0 +1,160 @@
+import p1 from "../../../assets/image/products/p1.png";
+import p2 from "../../../assets/image/products/p2.png";
+import p3 from "../../../assets/image/products/p3.png";
+import p4 from "../../../assets/image/products/p4.png";
+
+const ItemProduct = () => {
+  const product = [
+    {
+      uuid: 1,
+      image: p1,
+      product: "قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی",
+      amount: "175,000",
+      rating: 4,
+      offer: 0,
+    },
+    {
+      uuid: 2,
+      image: p2,
+      product: "قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی",
+      amount: "154,000",
+      rating: 5,
+      offer: 12,
+    },
+    {
+      uuid: 3,
+      image: p3,
+      product: "قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی",
+      amount: "-1",
+      rating: 3,
+      offer: 0,
+    },
+    {
+      uuid: 4,
+      image: p4,
+      product: "قهوه ترک بن مانو مقدار 250 گرم خط دوم اسم طولانی",
+      amount: "175,000",
+      rating: 4,
+      offer: 0,
+    },
+  ];
+
+  return (
+    <>
+      {product.map((products, index) => (
+        <section
+          key={index + product.uuid}
+          className="w-[300] h-[467px] p-5 relative bg-bgItemLightColor dark:bg-bgItemDarkColor flex flex-col justify-start items-center gap-4 rounded-2xl shadow-defaultShadow"
+        >
+          {products.offer != 0 && (
+            <span className=" absolute top-5 right-5 pt-1 px-3 text-base font-medium bg-primaryColor text-textPrimaryDarkColor dark:text-textPrimaryLightColor rounded-full flex justify-center items-center">
+              <p>{products.offer}%</p>
+            </span>
+          )}
+
+          <img
+            src={products.image}
+            alt={products.image}
+            className="w-full h-52 object-contain"
+          />
+
+          <div className="w-full flex flex-col justify-start items-start gap-3">
+            <h5 className="w-full text-right font-danamedium text-xl">
+              {products.product}
+            </h5>
+
+            <span
+              className={
+                products.amount != -1
+                  ? "w-full text-right font-medium text-xl text-successPrimaryColor"
+                  : "w-full text-right font-medium text-xl text-errorColor"
+              }
+            >
+              {products.amount != -1 ? products.amount : "فعلا موجود نیست"}
+              {products.amount != -1 && (
+                <span className="text-right text-sm font-normal"> تومان</span>
+              )}
+            </span>
+
+            <div className="w-full flex flex-row justify-between items-center">
+              <div className="flex flex-row justify-start items-center gap-3">
+                <span className="size-9 bg-gray-100 text-iconSecondaryColor rounded-full flex justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                    />
+                  </svg>
+                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 text-iconSecondaryColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+                  />
+                </svg>
+              </div>
+
+              <div className="flex flex-row justify-start items-center">
+                {[...Array(Number(5) - products.rating)].map((star) => (
+                  <>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6 text-iconSecondaryColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                      />
+                    </svg>
+                  </>
+                ))}
+
+                {[...Array(products.rating)].map((star) => (
+                  <>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke="currentColor"
+                      className="size-6 text-warningColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                      />
+                    </svg>
+                  </>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      ))}
+    </>
+  );
+};
+
+export default ItemProduct;
---
+++ frontend/src/components/title-section/TitleSection.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+
+// TitleSection.propTypes = {
+//   title: PropTypes.string
+//   subTitle: PropTypes.string
+//   toLink: PropTypes.string
+// }
+
+export default function TitleSection({ title, subTitle, toLink }) {
+  return (
+    <section className="w-full z-10 flex flex-row justify-between items-end">
+      <div className="flex flex-col justify-start items-start gap-3">
+        <h2 className="text-right font-bold text-5xl text-textPrimaryLightColor dark:text-textPrimaryDarkColor">
+          {title}
+        </h2>
+        <h3 className="text-right font-normal text-3xl text-textPrimaryLightColor dark:text-textPrimaryDarkColor">
+          {subTitle}
+        </h3>
+      </div>
+
+      <Link
+        to={toLink}
+        className="text-xl font-medium text-textSecondaryLightColor dark:text-textSecondaryDarkColor flex flex-row justify-center items-center gap-2"
+      >
+        مشاهده همه محصولات
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-5 h-5"
+        >
+          <path
+            fillRule="evenodd"
+            d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </Link>
+    </section>
+  );
+}
