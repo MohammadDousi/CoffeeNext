6567e851a6cb037515a01bdfaf3b365f4b2855e6
mohammad dousi <m.dousi96@gmail.com>
2024-05-09 21:45:53 +0330

    create categories item in home page , check empty link in title section components

--- frontend/src/components/title-section/TitleSection.jsx
+++ frontend/src/components/title-section/TitleSection.jsx
@@ -14,24 +14,28 @@ export default function TitleSection({ title, subTitle, toLink }) {
         </h3>
       </div>
 
-      <Link
-        to={toLink}
-        className="text-xl font-medium text-textSecondaryLightColor dark:text-textSecondaryDarkColor flex flex-row justify-center items-center gap-2 select-none"
-      >
-        مشاهده همه محصولات
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="w-5 h-5"
+      {toLink && (
+        <Link
+          to={toLink}
+          className="text-xl font-medium text-textSecondaryLightColor dark:text-textSecondaryDarkColor flex flex-row justify-center items-center gap-2 select-none"
         >
-          <path
-            fillRule="evenodd"
-            d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </Link>
+          مشاهده همه محصولات
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
+      )}
+
+
     </section>
   );
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,31 @@
+import PropTypes from "prop-types";
+
+// components
 import ItemProduct from "../components/product/item-product/item-product";
-
-import headerBgDesktop from "../assets/image/headerBgDesktop.webp";
-
-import farmer from "../assets/image/body-bg.png";
-import categoryLeft from "../assets/image/categories/category-left.jpg";
-import categoryRight from "../assets/image/categories/category-right.jpg";
 import TitleSection from "../components/title-section/TitleSection";
 
+// image
+import headerBgDesktop from "../assets/image/headerBgDesktop.webp";
+import farmer from "../assets/image/body-bg.png";
+
+// image category
+import categoryRight from "../assets/image/categories/category-right.jpg";
+import categoryLeft from "../assets/image/categories/category-left.jpg";
+import cat1 from "../assets/image/categories/category1.png";
+import cat2 from "../assets/image/categories/category2.png";
+import cat3 from "../assets/image/categories/category3.png";
+import cat4 from "../assets/image/categories/category4.png";
+import cat5 from "../assets/image/categories/category5.png";
+
 export default function HomePage() {
+  const categories = [
+    { uuid: 1, image: cat1, category_name: "قهوه دمی و اسپرسو" },
+    { uuid: 2, image: cat2, category_name: "لوازم جانبی و تجهیزات" },
+    { uuid: 3, image: cat3, category_name: "اسپرسو ساز" },
+    { uuid: 4, image: cat4, category_name: "پک تستر قهوه" },
+    { uuid: 5, image: cat5, category_name: "قهوه ترک" },
+  ];
+
   return (
     <main className="w-screen flex flex-col justify-center items-center">
       {/* image back landing and image farmer */}
@@ -18,7 +36,7 @@ export default function HomePage() {
         className="w-full h-auto xl:bg-contain bg-cover 2xl:bg-cover bg-no-repeat"
       />
 
-      <section className="w-screen relative pt-32 flex flex-col justify-center items-center gap-14">
+      <section className="w-[1260px] relative pt-32 flex flex-col justify-center items-center gap-16">
         <img
           src={farmer}
           alt="farmer"
@@ -28,7 +46,7 @@ export default function HomePage() {
         <TitleSection
           title={`جدیدترین محصولات`}
           subTitle={`فرآوری شده از دانه قهوه`}
-          toLink={``}
+          toLink={`#`}
         />
 
         <section className="grid grid-cols-[300px_300px_300px_300px] grid-rows-2 justify-between items-start gap-5">
@@ -71,7 +89,43 @@ export default function HomePage() {
             />
           </section>
         </section>
+
+        <section className="w-full grid grid-cols-[200px_200px_200px_200px_200px] grid-rows-1 justify-between items-start">
+          {categories.map((cat) => (
+            <div
+              className="size-full flex flex-col justify-center items-center gap-2"
+              key={cat.category_name}
+            >
+              <img
+                src={cat.image}
+                alt={cat.category_name}
+                className="size-[200px]"
+              />
+              <h4 className="text-xl font-bold">{cat.category_name}</h4>
+            </div>
+          ))}
+        </section>
+
+
+        <TitleSection
+          title={`محصولات پر فروش`}
+          subTitle={`پیشنهاد قهوه خور ها ...`}
+          toLink={`#`}
+        />
+
+        
+
+
       </section>
     </main>
   );
 }
+
+HomePage.propTypes = {
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({
+      uuid: PropTypes.string,
+      category_name: PropTypes.string,
+    })
+  ),
+};
