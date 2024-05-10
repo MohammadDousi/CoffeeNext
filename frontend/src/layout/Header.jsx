a02adaed087a6691b052a18e026e3e6c66167b1f
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2024-05-10 21:53:13 +0330

    fixed header

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
           <ul className='flex lg:gap-4 md:gap-3 xl:[&>*]:p-5 text-bgItemLightColor'>
             <li className='[&>*]:transition-colors [&>*]:hover:text-primaryColor'><Link to='/homepage'>صفحه اصلی</Link></li>
             <li className='relative group'><Link className="pb-5" to='/homepage'>فروشگاه</Link>
-              <ul className='flex flex-col dark:text-white text-black-submenu mt-5 bg-white dark:bg-bgDarkColor w-[208px] h-[272px] absolute right-[-20px] top-5 xl:top-10 2xl:top-[2.5rem] rounded-2xl justify-center gap-4 pr-5 invisible group-hover:visible transition-all'>
+              <ul className='flex flex-col dark:text-white text-black-submenu mt-5 bg-white dark:bg-bgDarkColor border-t-4 border-primaryColor w-[208px] h-[272px] absolute right-[-20px] top-5 xl:top-10 2xl:top-[2.5rem] rounded-2xl justify-center gap-4 pr-5 invisible group-hover:visible transition-all'>
                 <li className="[&>*]:transition-colors [&>*]:hover:text-primaryColor"><Link to='/shopping/1'>قهوه ویژه</Link></li>
                 <li className="[&>*]:transition-colors [&>*]:hover:text-primaryColor"><Link to='/shopping/2'>ویژه در سطح جهانی</Link></li>
                 <li className="[&>*]:transition-colors [&>*]:hover:text-primaryColor"><Link to='/shopping/3'>قهوه درجه یک</Link></li>
@@ -49,30 +49,32 @@ export default function Header() {
           </ul>
         </div>
         <div className="flex items-center gap-5 [&>*]:transition-colors">
-          <Link to='/basket' className="hover:text-primaryColor ">
+          <Link to='/basket' className="text-primaryColor ">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
             </svg>
           </Link>
           {
             theme == 'day' ?
-              <div onClick={nightHandler} className="hover:cursor-pointer hover:text-primaryColor">
+              <div onClick={nightHandler} className="hover:cursor-pointer text-primaryColor">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
                 </svg>
               </div>
               :
-              <div onClick={dayHandler} className="hover:cursor-pointer hover:text-primaryColor">
+              <div onClick={dayHandler} className="hover:cursor-pointer text-primaryColor">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
                 </svg>
 
               </div>
           }
-          <Link to='/register' className="hover:text-primaryColor">
+          <span className="bg-gray-600 w-[2px] h-12"></span>
+          <Link to='/register' className="hover:text-primaryColor flex gap-2 text-primaryColor">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
             </svg>
+            ورود | ثبت نام
           </Link>
         </div>
       </div>
