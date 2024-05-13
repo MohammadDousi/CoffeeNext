abb85db9be811a4381f49f0d05046ac07f07336f
mohammad dousi <m.dousi96@gmail.com>
2024-05-13 17:22:49 +0330

    delete route dir - merge route with app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Shopping from "./pages/Shopping";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Basket from "./pages/Basket";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
 import Layout from "./layout/Layout";
-import Routers from "./routes/Routers";
+
 function App() {
   return (
-    <>
-        <Layout>
-          <Routers />
-        </Layout>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shopping" element={<Shopping />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
+      </Routes>
+    </Layout>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
