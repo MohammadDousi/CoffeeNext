b6ba72161033c54aaeba8ef1e7c7a6da4cb9f556
mohammad dousi <m.dousi96@gmail.com>
2024-05-15 19:37:28 +0330

    delete svg file from image assets folder - create club section in home page and responsive for mobile and lg screen

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -38,6 +38,12 @@ import blogImage2 from "../assets/image/blogs/blog-2.png";
 import blogImage3 from "../assets/image/blogs/blog-3.png";
 import blogImage4 from "../assets/image/blogs/blog-4.png";
 
+// image club
+import diamond from "../assets/image/club/diamond.png";
+import activity from "../assets/image/club/Activity.svg";
+import discovery from "../assets/image/club/Discovery.svg";
+import ticketStar from "../assets/image/club/Ticket-Star.svg";
+
 // keen slider
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
@@ -227,6 +233,12 @@ export default function HomePage() {
     { uuid: "3", image: cat3, category_name: "اسپرسو ساز" },
     { uuid: "4", image: cat4, category_name: "پک تستر قهوه" },
     { uuid: "5", image: cat5, category_name: "قهوه ترک" },
+  ];
+
+  const club = [
+    { icon: activity, itemClub: "ماموریت ها" },
+    { icon: discovery, itemClub: "چرخ و بخت" },
+    { icon: ticketStar, itemClub: "جایزه ها" },
   ];
 
   // keen slider
@@ -522,6 +534,67 @@ export default function HomePage() {
             </span>
           </section>
 
+          {/* club */}
+          <section className="w-full h-64 lg:h-36 px-4 lg:px-11 bg-gradient-to-r from-successPrimaryColor to-successSecondaryColor flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-8 rounded-2xl">
+            <div className="w-full lg:w-1/2 flex flex-row justify-between lg:justify-start items-center gap-4 lg:gap-6">
+              <img
+                src={diamond}
+                alt="diamond"
+                className="w-[87px] lg:w-28 object-contain"
+              />
+              <div className="flex flex-col justify-start items-start lg:gap-2">
+                <h2 className="w-full font-[Morabba] font-bold text-2xl lg:text-5xl text-right text-textPrimaryDarkColor">
+                  کافی کلاب
+                </h2>
+                <h2 className="w-full font-[Morabba] font-light text-lg lg:text-2xl text-right text-textPrimaryDarkColor">
+                  میدونستی میتونی با امتیاز هات قهوه بگیری ؟
+                </h2>
+              </div>
+            </div>
+
+            <div className="w-full lg:w-1/2 flex flex-row justify-start items-center gap-3">
+              <div className="w-3/4 flex flex-row justify-start items-center gap-2 lg:gap-5">
+                {club.map((item, index) => (
+                  <div
+                    key={index}
+                    className="w-1/3 h-20 lg:size-24 text-xs lg:text-sm font-normal text-successSecondaryColor bg-bgItemLightColor rounded-2xl flex flex-col justify-center items-center gap-1.5 lg:gap-3"
+                  >
+                    <img
+                      src={item.icon}
+                      alt={`item ${item.icon}`}
+                      className="size-10 lg:size-12 object-contain"
+                    />
+                    {item.itemClub}
+                  </div>
+                ))}
+              </div>
+
+              <div className="w-1/4 h-20 lg:h-24 flex flex-col justify-between items-start">
+                <span className="w-full pr-2 text-2xl lg:text-3xl font-bold text-textPrimaryDarkColor">
+                  524
+                </span>
+                <span className="w-full pr-2 text-xs lg:text-sm font-normal text-textPrimaryDarkColor">
+                  امتیاز شما
+                </span>
+                <button className="w-full p-1 lg:px-2.5 lg:h-8 text-xs lg:text-sm font-normal text-center text-textPrimaryDarkColor bg-gradient-to-r from-primaryColor to-secondaryColor rounded-full flex flex-row justify-center items-center cursor-pointer">
+                  دریافت جایزه
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-4 lg:size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </section>
+
           <TitleSection
             title={`مطالب خواندنی`}
             subTitle={``}
@@ -582,8 +655,6 @@ export default function HomePage() {
               </button>
             </div>
           </section>
-
-          
         </section>
       </main>
     </>
