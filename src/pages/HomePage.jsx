567127ce263454969ed0bd135d3a365f13915b27
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2024-05-16 23:52:38 +0330

    delete lazyLoad component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -48,8 +48,6 @@ import ticketStar from "../assets/image/club/Ticket-Star.svg";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import ItemProduct from "../components/product/item-product/ItemProduct";
-import LazyLoad from "../components/lazyLoad/LazyLoad";
-import Loading from "../components/loading/Loading";
 
 // MarkdownPreview lazy load
 
@@ -291,12 +289,9 @@ export default function HomePage() {
 
   return (
     <>
-    <Loading />
       <main className="w-screen pt-16 lg:p-0 flex flex-col justify-center items-center">
         <div className="w-full lg:h-screen pb-6 relative flex flex-row justify-center items-center lg:items-center">
-          <LazyLoad>
              <img src={window.innerWidth <= 425 ? headerBgMobile : headerBgDesktop} alt="headerBgDesktop" className="w-full h-full object-contain lg:object-cover" />
-          </LazyLoad>
           <div className="hidden absolute bottom-6 z-40 lg:flex justify-center items-center">
             <svg
               width="100"
